90effa202baa7346d6493713abfd9e1afc1032c0
KseniiaHorai <horaiksenia@gmail.com>
2024-03-05 13:07:43 +0200

    Enhance mdConverter to support fenced code blocks

--- converter.js
+++ converter.js
@@ -4,11 +4,30 @@ function mdConverter(inputMd) {
     const italicPattern = /_(\S(?:.*?\S)?)_/g;
     const monospacedPattern = /`(\S(?:.*?\S)?)`/g;
 
-    inputMd = inputMd.replace(boldPattern, '<b>$1</b>');
-    inputMd = inputMd.replace(italicPattern, '<i>$1</i>');
-    inputMd = inputMd.replace(monospacedPattern, '<tt>$1</tt>');
+    let fencedCodeBlocks = [];
+    let outputHtml = inputMd;
 
-    let paragraphs = inputMd.split('\n{2,}');
+    let fencedCodeMatch;
+    let tempString = '';
+    let preIndex = 0;
+
+    while ((fencedCodeMatch = /(^\\n?|^)```(.*?)```(\\n?|$)/ms.exec(outputHtml)) !== null) {
+        let before = fencedCodeMatch[1];
+        let after = fencedCodeMatch[3];
+        fencedCodeBlocks.push(fencedCodeMatch[2]);
+        let tempRepl = (before.isEmpty ? '' : '\n') + 'replaceFencedCode' + preIndex++ + (after.isEmpty ? '' : '\n');
+        tempString += outputHtml.substring(0, fencedCodeMatch.index) + tempRepl;
+        outputHtml = outputHtml.substring(fencedCodeMatch.index + fencedCodeMatch[0].length);
+    }
+
+    outputHtml = tempString + outputHtml;
+
+    outputHtml = outputHtml.replace(boldPattern, '<b>$1</b>');
+    outputHtml = outputHtml.replace(italicPattern, '<i>$1</i>');
+    outputHtml = outputHtml.replace(monospacedPattern, '<tt>$1</tt>');
+
+
+    let paragraphs = outputHtml.split('\n{2,}');
 
     let outputBuilder = '';
     for (let paragraph of paragraphs) {
@@ -16,9 +35,12 @@ function mdConverter(inputMd) {
             outputBuilder += '<p>' + paragraph + '</p>\n';
         }
     }
-    inputMd = outputBuilder;
+    outputHtml = outputBuilder;
+    for (let i = 0; i < fencedCodeBlocks.length; i++) {
+        outputHtml = outputHtml.replace('replaceFencedCode' + i, '<pre>' + fencedCodeBlocks[i] + '</pre>');
+    }
     
-    return inputMd;
+    return outputHtml;
 }
 
 module.exports = { mdConverter };
--- input.md
+++ input.md
@@ -1,6 +1,18 @@
 **bold text**
 
+```
+This text is **preformatted**
+
+And can have multiple paragraphs
+```
+
+
 _italic fragment_
 
 `monospaced`
 
+```
+This text is **preformatted**
+
+And can have multiple paragraphs
+```
