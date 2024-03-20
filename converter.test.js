7d96bcaa372f0fc35ad0b39b4e8f25186dfd802f
KseniiaHorai <horaiksenia@gmail.com>
2024-03-20 22:40:29 +0200

    Add complex tests for HTML converting

--- converter.test.js
+++ converter.test.js
@@ -29,3 +29,48 @@ describe('Simple valid Markdown to HTML converting', () => {
   });
 
 });
+
+describe('Complex valid Markdown to HTML converting', () => {
+
+  test('Test snake_case', () => {
+    expect(mdConverter('It is ok to be snake_case', 'html')).toBe('<p>It is ok to be snake_case</p>\n');
+  });
+
+  test('Test single bold tag', () => {
+    expect(mdConverter('It is ok ** to be single', 'html')).toBe('<p>It is ok ** to be single</p>\n');
+  });
+
+  test('Test single italic tag', () => {
+    expect(mdConverter('It is ok _ to be single', 'html')).toBe('<p>It is ok _ to be single</p>\n');
+  });
+
+  test('Test single monospaced tag', () => {
+    expect(mdConverter('It is ok  ` to be single', 'html')).toBe('<p>It is ok  ` to be single</p>\n');
+  });
+
+  test('Test single preformatted tag', () => {
+    expect(mdConverter('It is ok ``` to be single', 'html')).toBe('<p>It is ok <tt>`</tt> to be single</p>\n');
+  });
+
+  test('Test complex case', () => {
+    expect(mdConverter([
+      'Test **bold** **bold1** **bold2**',
+      'Test _italic_ _italic1_ _italic2_',
+      'Test `monospaced` `monospaced1` `monospaced2`\n',
+      'It is ok to be snake_case',
+      'It is ok ** to be single\n',
+      '```\nThis text is **_preformatted_**\n',
+      'And can have multiple paragraphs\n```'
+    ].join('\n'), 'html'))
+    .toBe([
+      '<p>Test <b>bold</b> <b>bold1</b> <b>bold2</b>\n',
+      'Test <i>italic</i> <i>italic1</i> <i>italic2</i>\n',
+      'Test <tt>monospaced</tt> <tt>monospaced1</tt> <tt>monospaced2</tt></p>\n',
+      '<p>It is ok to be snake_case\n',
+      'It is ok ** to be single</p>\n',
+      '<p><pre>\nThis text is **_preformatted_**\n\n',
+      'And can have multiple paragraphs\n</pre></p>\n'
+    ].join(''))
+  });
+
+});
